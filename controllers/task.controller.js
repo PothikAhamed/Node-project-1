a618f6ed8a7424840ff31e942d8244ed1317afbd
PothikAhamed <cppothikkhan@gmail.com>
2026-09-16 01:54:46 +0600

    dataUpdate

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,20 +1,55 @@
 const productModel = require("./model/product.model");
 
-let add_task_controller = async (req, res)=>{
-    let add_product = await productModel.insertMany(req.body)
+let add_task_controller = async (req, res) => {
+  let add_product = await productModel.insertMany(req.body);
 
-    
-    res.status(201).json({
-        success:true,
-        message:"product created",
-        data:add_product})
-}
+  res.status(201).json({
+    success: true,
+    message: "product created",
+    data: add_product,
+  });
+};
 
-let getAllProductsController = async(req, res)=>{
-    let findProducts = await productModel.find({})
-    res.status(200).json({success: true, message:"data fetch successfully", data:findProducts})
-}
+let getAllProductsController = async (req, res) => {
+  let findProducts = await productModel.find({});
+  res
+    .status(200)
+    .json({
+      success: true,
+      message: "data fetch successfully",
+      data: findProducts,
+    });
+};
 
+let update_product_controller = async (req, res) => {
+  let products = await productModel.find({});
 
+  let updateProduct = await productModel.bulkWrite(
+    req.body.map((product, index) => ({
+      updateOne: {
+        filter: { _id: products[index]._id },
+        update: {
+          $set: {
+            title: product.title,
+            description: product.description,
+            category: product.category,
+            brand: product.brand,
+            price: product.price,
+          },
+        },
+      },
+    })),
+  );
 
-module.exports= {add_task_controller, getAllProductsController};
+  res.status(200).json({
+    success: true,
+    message: "data update successfully",
+    data: updateProduct,
+  });
+};
+
+module.exports = {
+  add_task_controller,
+  getAllProductsController,
+  update_product_controller,
+};
--- router/index.js
+++ router/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {add_task_controller, getAllProductsController} = require('../controllers/task.controller');
+const {add_task_controller, getAllProductsController, update_product_controller} = require('../controllers/task.controller');
 const router = express.Router()
 
 router.post("/add-task", add_task_controller)
 router.get("/producs", getAllProductsController)
+router.put("/productTwo", update_product_controller)
 
 
 
